f45e1776238a4c58e1e08dde3f6ffad3f2aa3b3d
miltonbanksy <miltonbanksy@gmail.com>
2026-04-11 22:39:16 +0900

    planets

---
+++ sf_planets.js
@@ -0,0 +1,18 @@
+sf_planetary_class = [
+    {
+        min: 1, max: 15,
+        pClass: "Desert World",
+        pDescription: "A pitiless planet of searing heat, blowing sand, and sunbaked rock.",
+        pAtmos: [
+            {aMin: 1, aMax: 10, aDescription: "None / Thin"},
+            {aMin: 11, aMax: 25, aDescription: "Toxic"},
+            {aMin: 26, aMax: 40, aDescription: "Corrosive"},
+            {aMin: 41, aMax: 70, aDescription: "Marginal"},
+            {aMin: 71, aMax: 95, aDescription: "Breathable"},
+            {aMin: 96, aMax: 100, aDescription: "Ideal"}
+        ],
+        pSettlements_terminus: [
+            {min: 1, max: 50, sDescription: "None"}
+        ]
+    }
+]
